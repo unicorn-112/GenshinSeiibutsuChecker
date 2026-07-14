c4be4bfdfc6596bc0568d0665750c4f77f51a8bc
unicorn-112 <rfqaze@gmail.com>
2026-07-14 18:52:47 +0900

    Update folder structures

---
+++ GenshinSeiibutsuChecker/src/main/java/com/artifact/score/artifactscoreapi/GenshinSeiibutsuCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.artifact.score.artifactscoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinSeiibutsuCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinSeiibutsuCheckerApplication.class, args);
+    }
+
+}
